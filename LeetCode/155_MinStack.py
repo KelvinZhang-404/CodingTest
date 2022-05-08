4048b7af4a402a5bb67af83223138beb31c984d6
Kelvin Zhang <lianxue1990@gmail.com>
2022-05-08 12:37:14 +1000

    155 min stack, use assistant stack

---
+++ LeetCode/155_MinStack.py
@@ -0,0 +1,44 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+#
+# Implement the MinStack class:
+#
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/min-stack
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# use assistant stack
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        if not self.minStack or self.minStack[-1] >= val:
+            self.minStack.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
